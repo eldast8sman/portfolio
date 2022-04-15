9e0654991c39ecad762a1215fc94abdbb3fdac54
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2022-04-15 15:56:54 +0100

    First Call to Action was populated

--- resources/views/components/home/first-action.blade.php
+++ resources/views/components/home/first-action.blade.php
@@ -1,3 +1,17 @@
-<div>
-    <!-- Very little is needed to make a happy life. - Marcus Aurelius -->
-</div>
+<x-call-to-action>
+    <x-slot:title>
+        <h4 class="text-white text-base font-semibold mb-2">
+            Need My Services?
+        </h4>
+        <h2 class="text-white font-bold text-3xl sm:text-[38px] leading-tight mb-6 sm:mb-8 lg:mb-0">
+            Need the Services <br class="hidden xs:block">
+            of a Seasoned Web Developer?
+        </h2>
+    </x-slot>
+    <x-button-link href="https://www.linkedin.com/in/omotolani-olurotimi-5b455b12a/" variant="dark" class="mr-3" target="_blank">
+        Check My LinkedIn
+    </x-button-link>
+    <x-button-link href="#contact" variant="red" target="_blank">
+        Reach out
+    </x-button-link>
+</x-call-to-action>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
     <x-home.hero></x-home.hero>
+    <x-home.first-action></x-home.first-action>
     <x-home.about></x-home.about>
 </x-app-layout>
